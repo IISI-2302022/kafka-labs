bb36380082cf1c5f7242652c59922c429f86fc19
mc.chen <mc.chen@iisigroup.com>
2026-02-28 14:26:17 +0800

    將 kafka install 之起訖 sh 放入

--- labs/01-kafka_install/start.sh
+++ labs/01-kafka_install/start.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+export THIS_SHELL_PATH="$(readlink -f "$0")"
+export THIS_SHELL_DIR="$(dirname "${THIS_SHELL_PATH}")"
+
+docker-compose -f "${THIS_SHELL_DIR}/docker-compose.yml" up -d --build
