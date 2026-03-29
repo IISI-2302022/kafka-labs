01c25c2b9f0dceb9c471584916a03247e8606c22
mc.chen <mc.chen@iisigroup.com>
2026-03-29 18:49:27 +0800

    update

--- labs/03-spring_kafka/spring-kafka/src/test/java/com/iisigroup/df/labs/admin/AdminTest.java
+++ labs/03-spring_kafka/spring-kafka/src/test/java/com/iisigroup/df/labs/admin/AdminTest.java
@@ -2,9 +2,7 @@ package com.iisigroup.df.labs.admin;
 
 import com.iisigroup.df.labs.config.MySpringBootTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -22,9 +20,6 @@ public class AdminTest {
         registry.add("spring_kafka_admin_auto_create", () -> true);
         registry.add("spring_kafka_bootstrap_servers", () -> "localhost:29092");
     }
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Test
     public void testAutoCreateTopic() throws InterruptedException, ExecutionException, TimeoutException {
