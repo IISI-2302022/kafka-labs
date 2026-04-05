dfeb80d05bc451a9fe11b8600ececfad853c5ac4
mc.chen <mc.chen@iisigroup.com>
2026-04-05 21:45:27 +0800

    update

--- labs/reset_restart.sh
+++ labs/reset_restart.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # 基本環境變數
-# 停用 MSYS(Git Bash) 自動路徑轉換（詳見 start.sh 說明）
-export MSYS_NO_PATHCONV=1
-
 export THIS_SHELL_PATH="$(readlink -f "$0")"
 export THIS_SHELL_DIR="$(dirname "${THIS_SHELL_PATH}")"
 
